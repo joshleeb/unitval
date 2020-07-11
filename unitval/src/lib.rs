dbbe884de93e477e8cd270fd65866a6732247c5c
Josh Leeb-du Toit <mail@joshleeb.com>
2020-07-11 21:40:13 +1000

    Add fns collect_as and collect_from

--- unitval/src/lib.rs
+++ unitval/src/lib.rs
@@ -1,12 +1,30 @@
 #[doc(hidden)]
 pub use unitval_derive::*;
 
-use std::io;
+use std::{io, iter::FromIterator};
 
 pub trait AsUnitVal {
     fn as_unitval(&self) -> &'static str;
 }
 
+pub fn collect_as<T, B>(value: &[T]) -> B
+where
+    T: AsUnitVal,
+    B: FromIterator<&'static str>,
+{
+    value.into_iter().map(|x| x.as_unitval()).collect()
+}
+
 pub trait FromUnitVal: Sized {
     fn from_unitval(value: &str) -> io::Result<Self>;
+}
+
+pub fn collect_from<T, I, B>(iter: I) -> io::Result<B>
+where
+    T: FromUnitVal,
+    I: IntoIterator,
+    I::Item: AsRef<str>,
+    B: FromIterator<T>,
+{
+    iter.into_iter().map(|x| FromUnitVal::from_unitval(x.as_ref())).collect()
 }
